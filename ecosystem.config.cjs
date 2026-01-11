888717b54061201e99fc5a7bdef4fc177510157b
Boris Abramov <borisab93@gmail.com>
2026-01-11 23:56:22 +0200

    add jwtsecret

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
         PORT: 3000,
         MONGODB_URI:
           "mongodb+srv://adamaviv93_db_user:JeF9MmzCyPTstgb5@main-cluster.9vydytk.mongodb.net/calorie-calculator?appName=main-cluster",
+        JWT_SECRET: "mynameisadamavivandiwasbornin1993",
       },
     },
   ],
